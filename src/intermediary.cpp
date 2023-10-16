4d3055f637fcf76c1c0621027ac0c9d23c7afaf3
Jaaziel21 <A01749505@tec.mx>
2023-10-16 12:41:04 -0600

    intermediary.cpp

--- src/intermediary.cpp
+++ src/intermediary.cpp
@@ -3,20 +3,20 @@
 #include "std_msgs/msg/float64.hpp"
 
 using std::placeholders::_1;
-using Twist   = geometry_msgs::msg::Twist;
-using Float64 = std_msgs::msgs::Float64;
+using Twist = geometry_msgs::msg::Twist;
+
 
 class IntermediateNode : public rclcpp::Node {
 
 private: 
 
-	    rclcpp::Subscription<Twist>::SharedPtr subscription_drive_teleop;
-        rclcpp::Subscription<Float64>::SharedPtr subscription_angles;
+	 rclcpp::Subscription<Twist>::SharedPtr subscription_drive_teleop;
+        rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_angles;
 
-    	rclcpp::Publisher<Float64>::SharedPtr publisher_front_left;
-    	rclcpp::Publisher<Float64>::SharedPtr publisher_front_right;
-    	rclcpp::Publisher<Float64>::SharedPtr publisher_back_left;
-    	rclcpp::Publisher<Float64>::SharedPtr publisher_back_right;
+    	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_front_left;
+    	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_front_right;
+    	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_back_left;
+    	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_back_right;
 
         float frontLeftAngle  = 0.0;
         float frontRightAngle = 0.0;
@@ -29,12 +29,12 @@ public:
         IntermediateNode() : Node("intermediate_node") {
         
         subscription_drive_teleop = this->create_subscription<Twist>("/cmd_vel", 10, std::bind(&IntermediateNode::driveTeleopCallback, this, _1));
-        subscription_angles = this->create_subscription<Float64>("/angulos", 10, std::bind(&intermediateNode::anglesCallback, this, _1));
+        subscription_angles = this->create_subscription<std_msgs::msg::Float64>("/angulos", 10, std::bind(&IntermediateNode::anglesCallback, this, _1));
 
-        publisher_front_left  = this->create_publisher<Float64>("/swerve/front_left", 10);
-        publisher_front_right = this->create_publisher<Float64>("/swerve/front_right", 10);
-        publisher_back_left   = this->create_publisher<Float64>("/swerve/back_left", 10);
-        publisher_back_right  = this->create_publisher<Float64>("/swerve/back_right", 10);
+        publisher_front_left  = this->create_publisher<std_msgs::msg::Float64>("/swerve/front_left", 10);
+        publisher_front_right = this->create_publisher<std_msgs::msg::Float64>("/swerve/front_right", 10);
+        publisher_back_left   = this->create_publisher<std_msgs::msg::Float64>("/swerve/back_left", 10);
+        publisher_back_right  = this->create_publisher<std_msgs::msg::Float64>("/swerve/back_right", 10);
     }
 
     	void driveTeleopCallback(const Twist::SharedPtr msg) {
@@ -61,14 +61,14 @@ public:
             }
     }
 
-        void anglesCallback(const Float64::SharedPtr msg){
+        void anglesCallback(const std_msgs::msg::Float64::SharedPtr msg){
         
             if(twist.linear.y == 0  and twist.angular.z == 0){
 
-                frontLeftAngle  = msg;
-                frontRightAngle = msg;
-                backLeftAngle   = msg;
-                backRightAngle  = msg;
+                frontLeftAngle  = static_cast<float>(msg->data);
+                frontRightAngle = static_cast<float>(msg->data);
+                backLeftAngle   = static_cast<float>(msg->data);
+                backRightAngle  = static_cast<float>(msg->data);
                 publish_angles();
             }
 
@@ -76,19 +76,19 @@ public:
 
     void publish_angles(){
 
-            Float64 frontLeftMsg; 
+            std_msgs::msg::Float64 frontLeftMsg; 
             frontLeftMsg.data = frontLeftAngle;
             publisher_front_left->publish(frontLeftMsg);
 
-            Float64 frontRightMsg;
+            std_msgs::msg::Float64 frontRightMsg;
             frontRightMsg.data = frontRightAngle;
             publisher_front_right->publish(frontRightMsg);
 
-            Float64 backLeftMsg;
+            std_msgs::msg::Float64 backLeftMsg;
             backLeftMsg.data = backLeftAngle;
             publisher_back_left->publish(backLeftMsg);
 
-            Float64 backRightMsg;
+            std_msgs::msg::Float64 backRightMsg;
             backRightMsg.data = backRightAngle;
             publisher_back_right->publish(backRightMsg);
     }
