da247e4c154a5f37b452b594ac419674b0824446
Ximeniucs654 <ximenachevalier15@gmail.com>
2023-10-23 12:05:43 -0600

    documentación del intermediario hecha

--- src/intermediary.cpp
+++ src/intermediary.cpp
@@ -1,33 +1,35 @@
+//Librerias de ROS
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include "std_msgs/msg/float64.hpp"
+#include "geometry_msgs/msg/twist.hpp" //velocidades angulares en ROS
+#include "std_msgs/msg/float64.hpp" //tipo de float
 
-using std::placeholders::_1;
-using Twist = geometry_msgs::msg::Twist;
+using std::placeholders::_1; // reservar un espacio
+using Twist = geometry_msgs::msg::Twist; // utilizar velocidad angular 
 
 
 class IntermediateNode : public rclcpp::Node {
 
 private: 
 
-	 rclcpp::Subscription<Twist>::SharedPtr subscription_drive_teleop;
-        rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_angles;
+	 rclcpp::Subscription<Twist>::SharedPtr subscription_drive_teleop; // puntero (subscripition)
+        rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_angles; // puntero (subscription)
 
-    	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_front_left;
+        rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_front_left; // apuntadores especificos
     	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_front_right;
     	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_back_left;
     	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_back_right;
 
-        float frontLeftAngle  = 0.0;
+        float frontLeftAngle  = 0.0; // dandole valores a las variables a base de un FLoat
         float frontRightAngle = 0.0;
         float backLeftAngle   = 0.0;
         float backRightAngle  = 0.0;
-        Twist twist;
+        Twist twist; 
 
 
 public:
-        IntermediateNode() : Node("intermediate_node") {
-        
+        // se mada a llamar a la funcion Intermidiate node
+        IntermediateNode() : Node("intermediate_node") {  
+        // a esta variable se le ingresa el valor que va después del apuntador
         subscription_drive_teleop = this->create_subscription<Twist>("/cmd_vel", 10, std::bind(&IntermediateNode::driveTeleopCallback, this, _1));
         subscription_angles = this->create_subscription<std_msgs::msg::Float64>("/angle_swr", 10, std::bind(&IntermediateNode::anglesCallback, this, _1));
 
@@ -36,13 +38,15 @@ public:
         publisher_back_left   = this->create_publisher<std_msgs::msg::Float64>("/swerve/back_left", 10);
         publisher_back_right  = this->create_publisher<std_msgs::msg::Float64>("/swerve/back_right", 10);
     }
-
+        // funciones que no regresan cosas pero si cumplen una funcion
     	void driveTeleopCallback(const Twist::SharedPtr msg) {
-        
+
+            // se le agrega el valor del mensaje a los linears
             twist.linear.y  = msg->linear.y;
             twist.linear.x  = msg->linear.x;
             twist.angular.z = msg->angular.z;
-
+            
+            // condiciones del control
             if (msg->linear.y != 0 and msg->angular.z == 0){
                 
                 frontLeftAngle  = 90.0;
@@ -60,7 +64,7 @@ public:
                 publish_angles();
             }
     }
-
+        
         void anglesCallback(const std_msgs::msg::Float64::SharedPtr msg){
         
             if(twist.linear.y == 0  and twist.angular.z == 0){
@@ -73,7 +77,7 @@ public:
             }
 
         }
-
+    // PUBLICA ANGULOS
     void publish_angles(){
 
             std_msgs::msg::Float64 frontLeftMsg; 
